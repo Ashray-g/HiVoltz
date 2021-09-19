f00c55dc53c19d46802fab754caea2396b1d9286
Ocasu <ocasumc@gmail.com>
2021-09-19 16:05:27 -0700

    Another tiny bug fix luca

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         init();
-
+        SwingController.initAll();
     }
 
     public static void init() throws IOException {
-        BoardControl.init(25, 25);
+        BoardControl.init(12, 12);
         BoardControl.spawnFences();
         BoardControl.spawnMhos();
         BoardControl.spawnPlayer();
         BoardControl.print();
-        SwingController.initAll();
     }
 }
